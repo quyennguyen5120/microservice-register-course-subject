2ac41c17dadeded163540d42d1395b7a85239f5c
QuyenNguyen <nguyenquyen5120@gmail.com>
2024-11-12 16:07:22 +0700

    Create service-auth

--- service-auth/src/main/java/com/auth/service/ServiceAuthApplication.java
+++ service-auth/src/main/java/com/auth/service/ServiceAuthApplication.java
@@ -1,11 +1,13 @@
-package com.example;
+package com.auth.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class ServiceAuthApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(ServiceAuthApplication.class, args);
     }
+
 }
---
+++ service-auth/src/test/java/com/auth/service/ServiceAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auth.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
