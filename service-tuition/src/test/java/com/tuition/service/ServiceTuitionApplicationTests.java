a17d603d2fe515ecd610f40a089b3a1c2883e5be
Quyen Nguyen <nguyenquyen5120@gmail.com>
2024-12-08 19:02:21 +0700

    Feat: Create service-tuition

---
+++ service-tuition/src/test/java/com/tuition/service/ServiceTuitionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuition.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceTuitionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
