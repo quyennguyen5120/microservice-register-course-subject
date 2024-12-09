9f0f0ca4594c01d79dc6b5f9105886db9c4e2788
QuyenNguyen <nguyenquyen5120@gmail.com>
2024-12-09 17:16:57 +0700

    Feat: config gRPC

--- service-auth/src/main/java/com/auth/service/controller/HelloController.java
+++ service-auth/src/main/java/com/auth/service/controller/HelloController.java
@@ -1,16 +1,16 @@
 package com.auth.service.controller;
 
 import com.stubs.grpc.HelloRequest;
+import com.stubs.grpc.HelloResponse;
 import com.stubs.grpc.HelloServiceGrpc;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
 
-@RestController
-public class HelloController {
 
-    @GetMapping("/")
-    public String test(){
-        HelloServiceGrpc.HelloServiceBlockingStub stub = HelloServiceGrpc.newBlockingStub(null);
-        return "Hello from auth service";
+@GrpcService
+public class HelloController extends HelloServiceGrpc.HelloServiceImplBase {
+    @Override
+    public void sayHello(HelloRequest request, StreamObserver<HelloResponse> responseObserver) {
+        System.out.println("Received message: " + request.getName());
     }
 }
